9f3744a3f5d7690fdce7db01692e6d3a77d5938b
Behnam Hajian <b.hajian@gmail.com>
2020-04-10 23:25:16 -0400

    add market data project

---
+++ MarketData.py
@@ -0,0 +1,5 @@
+import market.FinancialMarketData as fd
+import numpy as np
+
+data = fd.forex()
+print(data)
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='marketData',
+    version='0.1.0',
+    author='Behnam Hajian',
+    description= 'This module is to retrieve market data',
+    author_email='bhajian@gmail.com',
+    license='MIT',
+    packages='marketData',
+    package_dir={'': 'marketData'}
+)
+
